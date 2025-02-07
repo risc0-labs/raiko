aff6aa368d7f1c17c87b493306689a2abe8c341f
Austin Abell <austinabell8@gmail.com>
2025-02-07 14:36:01 -0500

    changes to profile paging

--- script/build.sh
+++ script/build.sh
@@ -3,9 +3,9 @@
 # Any error will result in failure
 set -e
 
-TOOLCHAIN_RISC0=+nightly-2024-09-05
-TOOLCHAIN_SP1=+nightly-2024-09-05
-TOOLCHAIN_SGX=+nightly-2024-09-05
+TOOLCHAIN_RISC0=+nightly
+TOOLCHAIN_SP1=+nightly
+TOOLCHAIN_SGX=+nightly
 
 check_toolchain() {
     local TOOLCHAIN=$1
